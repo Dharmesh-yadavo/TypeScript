7e8dd1990de237c6d1cf3d9511bcf48afc5289c3
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-09-21 01:25:05 +0530

    object code added

---
+++ object.js
@@ -0,0 +1,47 @@
+//* In TypeScript, objects are used to represent data with key-value pairs.
+//* Each key in the object is a string (or a symbol in ES6) that maps to a value.
+// ? Let's consider a real-life example of representing a person's information using TypeScript objects
+// ? ℹ️ -> name, age, isStudent, address{ city, country }
+var person = {
+    name: "Dharmesh Yadav",
+    age: 21,
+    isStudent: true,
+    address: {
+        city: "delhi",
+        country: "India",
+    },
+};
+var person1 = {
+    name: "Dheeraj Yadav",
+    age: 21,
+    isStudent: true,
+    address: {
+        city: "delhi",
+        country: "India",
+    },
+};
+var person2 = {
+    name: "Kunal",
+    age: 16,
+    isStudent: true,
+    address: {
+        city: "haryana",
+        country: "India",
+    },
+};
+// Todo Accessing the object data
+console.log(person.address.country);
+// Todo Updatingobject properties
+person.address.city = "pune";
+console.log(person.address.city);
+//! Homework
+//?Create a Product Object:
+//Define a product with properties for name, price, and quantity.Create a product object with the following data:
+// Name: "Laptop"
+// Price: 1000
+// Quantity: 5
+var product = {
+    name: "Laptop",
+    price: 1000,
+    quantity: 5,
+};
