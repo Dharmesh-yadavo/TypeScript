42b210dc5f5a41779188fd29d3b4f681c2af07ae
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-09-20 20:18:44 +0530

    intro of Array coded

---
+++ intro_array.js
@@ -0,0 +1,14 @@
+//! array in TS
+//? Using square brackets
+var number = [1, 2, 3, 4, 5];
+//? Using array constructor
+var number1 = new Array(10, 20, 30, 40, 50);
+//? Using the Array.of method
+var name = Array.of("dharm", "yadav", "mohit");
+//* Operation: Acessing elements using index
+console.log(name[2]);
+console.log(name.length);
+// TODO Homework Time
+// different types of array: example
+// const value1 = [1, 2, 3, 4, 5];
+// const value2 = [true, false, true];
---
+++ intro_array.ts
@@ -0,0 +1,20 @@
+//! array in TS
+
+//? Using square brackets
+const number: number[] = [1, 2, 3, 4, 5];
+
+//? Using array constructor
+const number1: number[] = new Array(10, 20, 30, 40, 50);
+
+//? Using the Array.of method
+const names: string[] = Array.of("dharm", "yadav", "mohit");
+
+//* Operation: Acessing elements using index
+console.log(names[2]);
+console.log(names.length);
+
+// TODO Homework Time
+// different types of array: example
+
+// const value1 = [1, 2, 3, 4, 5];
+// const value2 = [true, false, true];
