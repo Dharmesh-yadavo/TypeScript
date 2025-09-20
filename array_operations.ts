ae0a97bc7325b5d78d1a92f479685b18893e8d2f
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-09-20 20:54:40 +0530

    array operations code done

---
+++ array_operations.ts
@@ -0,0 +1,28 @@
+//? Arrays in TypeScript come with built-in methods that allow you to perform common operations
+
+const fruits: string[] = ["apple", "banana", "orange", "mango"];
+
+// a) Adding elements to an array using push:
+const newUpdatedFruits = fruits.push("Kiwi");
+console.log(newUpdatedFruits); // 5
+console.log(fruits);
+
+// b) removing elemts from the array
+const lastData = fruits.pop();
+console.log(lastData); // Kiwi
+
+//! Iterating over elements
+// ? You can iterate over the elements of an array using various looping constructs such as for, for...of, or array methods like forEach.
+
+// Using for loop
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// Using for of loop
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+// Using forEach loop
+fruits.forEach((curVal: string) => console.log(curVal));
