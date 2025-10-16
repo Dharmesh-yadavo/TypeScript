63649fb1ba5f5749a918bba46216e95ac68c956a
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-10-17 00:43:47 +0530

    #31: typeGuard

---
+++ dist/31_typeGuard.js
@@ -0,0 +1,18 @@
+"use strict";
+// A typeof guard in Typescript lets you narrow down the type of a variable based on its runtime value.
+// In Typescript, type narrowing allows you to write type-safety code by ensuring you only operate on
+//  the correct type under certain circumstances. This is particularly useful with union types and
+//  generic types.
+Object.defineProperty(exports, "__esModule", { value: true });
+const favHobbies = (hobby) => {
+    // return hobby.map(() => { })
+    //Property 'map' does not exist on type 'string | string[] '.Property 'map' does not exist on type 'string'.
+    if (typeof hobby === "object" && Array.isArray(hobby)) {
+        return hobby.map((hobby) => hobby);
+    }
+    else {
+        return hobby;
+    }
+};
+console.log(favHobbies("coding"));
+console.log(favHobbies(["coding", "palying", "movies"]));
